703075467ec839732a3de437ec0e42fa47c14b6e
shamim51 <43220289+shamim51@users.noreply.github.com>
2023-08-19 10:35:17 +0600

    Update app.js

--- static/app.js
+++ static/app.js
@@ -62,7 +62,7 @@ class Chatbox {
         sendButton.disabled = true;
         sendButton.textContent = "Sending...";
     
-        fetch('http://127.0.0.1:8000/predict', {
+        fetch('https://ebot.onrender.com/predict', {
             method: 'POST',
             body: JSON.stringify({ message: userMessage }),
             mode: 'cors',
